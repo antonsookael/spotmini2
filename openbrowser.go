637275f470693d83071129b67b08e93d7f74527a
antonsookael <anton.sookael@gmail.com>
2026-07-29 17:19:14 +0300

    fixed openbrowser on mac

--- openbrowser.go
+++ openbrowser.go
@@ -3,14 +3,24 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"runtime"
 )
 
 // openBrowser launches the system's default browser to the given URL.
-// "start" is a Windows-specific shell command for opening things with
-// their default program - on Mac this would be "open", on Linux
-// typically "xdg-open".
+// The command differs per OS: Windows needs "cmd /c start", macOS uses
+// "open", and Linux typically has "xdg-open".
 func openBrowser(url string) {
-	cmd := exec.Command("cmd", "/c", "start", "", url)
+	var cmd *exec.Cmd
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = exec.Command("cmd", "/c", "start", "", url)
+	case "darwin":
+		cmd = exec.Command("open", url)
+	default: // linux and other unix-likes
+		cmd = exec.Command("xdg-open", url)
+	}
+
 	if err := cmd.Start(); err != nil {
 		fmt.Println("Couldn't open browser automatically:", err)
 		fmt.Println("Open this URL manually:", url)
